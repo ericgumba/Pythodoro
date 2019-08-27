0b18168d3cd472e0d5864613cc4d606b075ac7d5
okmanl <ericgumba@gmail.com>
2019-08-26 22:34:31 -0700

    added pomodoro object

--- pomodoro.py
+++ pomodoro.py
@@ -4,67 +4,25 @@ import sched
 import threading
 import time 
 import journal_writer 
+import json 
 
 class Pomodoro:
-    def __init__(self, task = None):
+    def __init__(self, settings, journalWriter: journal_writer.JournalWriter = journal_writer.JournalWriter()):  
+        self.workDuration = settings["workDuration"]
+        self.breakDuration = settings["breakDuration"]
+        self.soundOn = settings["soundOn"]
+        self.pomodoroModeOn = settings["pomodoroModeOn"] 
         self.isWorking = True
-        self.task = task
-        self.journalWriter = journal_writer.JournalWriter(task)
+        self.journalWriter = journalWriter
          
     def writeToJournal(self): 
-        journalWriter.write()
-
-    def getBackToWork(self):
-        print("back to work")
-
-
+        journalWriter.write() 
 
     def switchPomodoro(self):  
         if self.isWorking:
-            self.takeABreak()
+            self.isWorking = False
         else: 
-            self.work()
-
-    def work(self):
-        self.isWorking = not self.isWorking
-        self.writeToJournal()
-
-
-    def takeABreak(self): 
-        self.isWorking = True
-        self.getBackToWork()
-
-  
-
-
-def startPomodoro():
-    if pom.isWorking:  
-        pom.switchPomodoro() 
-        # os.system('say "break time."')  
-    else:
-        pom.switchPomodoro()
-        # os.system('say "back to work."')
-
-pom = Pomodoro("Love")
+            self.isWorking = True
+            journalWriter.write() 
  
-
-
-def pomodoroStart( workDuration ):        
-    count = 0
-    while count < workDuration:
-       time.sleep(1)
-       count += 1
-    if pom.isWorking:
-        pom.switchPomodoro()
-        os.system( 'say "break time."' )
-    else:
-        os.system( 'say "back to work"' ) 
-        pom.switchPomodoro()
-
-
-def startThread():
-    while True:
-    	pomodoroStart( 25 )
-    	pomodoroStart( 5  )
-
-startThread()
+ 
